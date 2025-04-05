8ac9dbb82a0d4be57ba4f0406414fa3015063569
StevenFeng <StevenFeng1020@outlook.com>
2025-04-06 02:09:16 +1100

    feat: new project

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      // 可在此扩展自定义颜色、字体等
+    },
+  },
+  plugins: [],
+}
